6089bfd016ee6abbc7c597f7f7d2d6834791a289
PavloShutz <pavlo050706@gmail.com>
2023-03-17 08:22:48 +0200

    added authentication and token getting for user model

--- db.py
+++ db.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+
+from flask_jwt_extended import create_access_token
 from passlib.hash import pbkdf2_sha256
 from sqlalchemy import String, create_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -21,6 +24,20 @@ class User(Base):
       self.name = name
       self.email = email
       self.password = pbkdf2_sha256.hash(password)
+    
+    def get_token(self, expire_time=24):
+       expire_delta = timedelta(expire_time)
+       token = create_access_token(
+          identity=self.id, expires_delta=expire_delta
+        )
+       return token
+    
+    @classmethod
+    def authenticate(cls, email, password):
+       user = cls.query.filter(cls.email == email).one()
+       if not pbkdf2_sha256.verify(password, user.password):
+          raise Exception("No user with this password")
+       return user
 
 
 Base.metadata.create_all(engine)
